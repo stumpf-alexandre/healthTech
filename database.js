af9c6555f5a3cd5dee8f8ab06ebc42c9bc9cf524
Alexandre <als.stumpf@gmail.com>
2026-03-26 21:44:58 -0300

    importando sqlite e sqlite3

---
+++ database.js
@@ -0,0 +1,5 @@
+//====================================================
+//Passo-1, importar sqlite(cerebro) e sqlite3(musculo)
+//====================================================
+const sqlite3 = require('aqlite3');
+const {open} = require('sqlite');
