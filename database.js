3874ae3077f178c59d4e38bda4eaffdccde7667b
Alexandre <als.stumpf@gmail.com>
2026-03-26 22:34:02 -0300

    criar tabelas pai e filhos do BD

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ const {open} = require('sqlite');
 //====================================================
 //Passo-2, criando função assincrona
 //====================================================
-const createBank = async() => {
+const criarBanco = async() => {
 
 //====================================================
 //Passo-3, cria o banco de dados
@@ -16,4 +16,56 @@ const createBank = async() => {
         filename: './database.db',
         driver: sqlite3.Database
     });
+
+//====================================================
+//Passo-4, criar tabelas do BD
+//=========v==========================================
+
+    await db.exec("PRAGMA foreign_keys = ON;");
+
+    //Tabela pai
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS pacientes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome_paciente TEXT,
+            endereco_paciente TEXT,
+            mobilidade_paciente TEXT,
+            nome_responsavel TEXT,
+            telefone_responsavel TEXT,
+            historico_saude TEXT,
+        )
+    `);
+
+    //Tabela filho, registro_saude
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS registro_saude (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            paciente_id INTEGER,
+            data_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
+            medicacao TEXT,
+            hora_medicacao TEXT,
+            exercicio_especifico TEXT,
+            humor TEXT,
+            sinais_vitais TEXT,
+            apetite TEXT,
+            FOREIGN KEY (paciente_id) REFERENCES pacientes(id) ON DELETE CASCADE
+        )
+    `);
+
+    //Tabela filho registro_higiene
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS registro_higiene (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            paciente_id INTEGER,
+            data_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
+            tipo_banho TEXT,
+            higiene_bucal BOOLEAN,
+            troca_fralda BOOLEAN,
+            hidratacao_pele TEXT,
+            observacoes TEXT  DEFAULT 'Sem observações',
+            FOREIGN KEY (paciente_id) REFERENCES pacientes(id) ON DELETE CASCADE
+        )
+    `);
+    console.log('Banco de dados configurado!!!');
+
 };
