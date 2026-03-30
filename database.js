70247455a5b55b98fce8a9917a5ab3fa19cfcd21
Alexandre <als.stumpf@gmail.com>
2026-03-29 22:33:32 -0300

    corrigindo bug criarBanco pasta server e pasta database

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ const {open} = require('sqlite');
 //====================================================
 //Passo-2, criando função assincrona
 //====================================================
-const createBank = async() => {
+const criarBanco = async () => {
 
 //====================================================
 //Passo-3, cria o banco de dados
@@ -35,10 +35,40 @@ const createBank = async() => {
             higiene_bucal INTEGER,
             troca_fralda INTEGER,
             hidratacao_pele TEXT,
-            observacoes TEXT  DEFAULT 'Sem observações',
+            observacoes TEXT DEFAULT 'Sem observações'
         )
     `);
 
     console.log('Banco de dados configurado!!!');
 
+//====================================================
+//Passo-10, Insert (C_rud 'CREATE')
+//====================================================
+
+    const checagem = await db.get('SELECT COUNT(*) AS total FROM pacientes');
+
+    if(checagem.total === 0) {
+        await db.exec(`
+            INSERT INTO pacientes(nome_paciente, endereco_paciente, nome_responsavel, telefone_responsavel, medicação_paciente, hora_medicacao, exercicio_especifico, tipo_banho, higiene_bucal, troca_fralda, hidratacao_pele) VALUES 
+                ('João Banguela', 'Rua Aparecida, 3422, bairro: Centro', 'João Banguela Filho', '(22)929292929', 'Lorazepa, 1 comprimido', '8:00', 'Fortalecimento muscular', 'Banho de asperção', 1, 0,'sem hidratação'),
+                ('Lurdez Munhoz', 'Rua Principal, 244, bairro: Morro da peruca', 'Ivone Munhoz Retzz', '(21)932231233', 'Pantoprazol, 1 comprimido', '12:00', 'Atividade cognitiva', 'Banho parcial', 0, 0,'hidratação de pernas'),
+                ('Raul Assunção', 'Rua Nova, 102, bairro: Praia Grande', 'Rubia Assunção', '(21)989898980', 'Atenolol, 1 1/2 comprimido', '10:00', 'Exercicio Passivo Ativo', 'Banho de leito', 1, 1,'hidratação de membros posteriores e inferiores'),
+                ('Bianca Fortuna', 'Rua Aurelio Paz, 1567, bairro: Centro', 'Alison Fortuna', '(21)988877220', 'Vertix, 1 comprimido', '13:45', 'Exercicio Cognitivo e Funcional', 'Banho de leito', 1, 1,'hidratação de articulações')
+        `);
+    } else {
+        console.log(`Banco de dados pronto com ${checagem.total} pacientes`);
+    }
+
+//====================================================
+//Passo-11, Select (c_R_ud 'READ')
+//====================================================
+    const todosPacientes = await db.all(`
+            SELECT * FROM pacientes
+        `);
+    console.table(todosPacientes);
+
+
+    return db;
 };
+
+module.exports = {criarBanco};
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //Passo-5, Importando Express e BD
 //====================================================
 const express = require ('express');
-const createBanck = require('./database');
+const {criarBanco} = require('./database');
 
 //====================================================
 //Passo-6, inicializando e configurando express
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
                 <h1>Health Tech</h1>
                 <h2>Gestão de cuidados para idosos</h2>
                 <p>Endpoint que leva aos pacientes cadastrados: <a href="http://localhost:3000/pacientes">/pacientes</a></p>
-                <p>Endpoint que leva aos incidentes cadastrados: <a href="http://localhost:3000/pacientes/4">/pacientes especifico</a></p>
+                <p>Endpoint que leva aos paciente cadastrados: <a href="http://localhost:3000/pacientes/4">/paciente especifico</a></p>
             </body>
         `);
 });
@@ -36,7 +36,7 @@ app.listen(PORT, () => {
 //Passo-9, Rota de listagem
 //====================================================
 app.get('/pacientes', async (req, res) => {
-    const db = await createBanck();
-    const listPatients = await db.all(`SELECT * FROM pacientes`);
-    res.json(listPatients);
+    const db = await criarBanco();
+    const listarPacientes = await db.all(`SELECT * FROM pacientes`);
+    res.json(listarPacientes);
 });
